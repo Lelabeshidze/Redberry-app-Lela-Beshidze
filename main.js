1f92bb10c2138177ec6cf354d800cf28df698860
Lelabeshidze <lela.beshidze@gmail.com>
2022-03-07 00:04:25 +0400

    just try smthng

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function myFunction() {
-    const x = document.getElementById("Demo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
+document.getElementById("next").onclick = function () {
+    location.href = 'skills.html';
+};
